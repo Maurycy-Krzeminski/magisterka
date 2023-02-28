8e42edd34d4ccd19b8c4b88b01933072ac3d06d2
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-02-28 12:57:49 +0100

    MATH - adding integration test for EquationResourceTest

---
+++ math/src/native-test/kotlin/org/maurycy/framework/reosurce/EquationResourceTestIT.kt
@@ -0,0 +1,7 @@
+package org.maurycy.framework.reosurce
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+import org.maurycy.framework.math.resource.EquationResourceTest
+
+@QuarkusIntegrationTest
+class EquationResourceTestIT: EquationResourceTest()
