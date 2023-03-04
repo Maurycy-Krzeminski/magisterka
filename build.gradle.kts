b4b2c8f13603cca60cc93654c930ba5741d9c3d2
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-03-04 15:56:45 +0100

    COMPOSITE - setting group for tasks in composite build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ group = "org.maurycy.framework"
 version = "1.0.0-SNAPSHOT"
 
 tasks.register("buildFull"){
+    group="local"
     dependsOn(gradle.includedBuilds.map { it.task(":build") })
 }
 
 tasks.register("clean") {
+    group="local"
     dependsOn(gradle.includedBuilds.map { it.task(":clean") })
 }
