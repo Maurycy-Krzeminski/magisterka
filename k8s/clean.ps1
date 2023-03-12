63d46d819da06b84e39c5ac4210d2e99421714f9
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-03-12 08:09:11 +0100

    GENERAL - removal of k8s scripts

--- k8s/clean.ps1
+++
@@ -1,2 +0,0 @@
-k3d cluster delete newcluster
-k3d register delete myregistry.localhost
--- k8s/start.ps1
+++
@@ -1,6 +0,0 @@
-k3d registry create myregistry.localhost --port 12345
-k3d cluster create newcluster --registry-use k3d-myregistry.localhost:12345 --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer
-
-../gradlew clean -p ../
-
-../gradlew buildFull -p ../ -D'quarkus.package.type'=native -D'quarkus.profile'=native
