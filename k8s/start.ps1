417a72e9c347d80452024e1aff07c9e75b84b089
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-03-13 20:53:26 +0100

    K8S - basic script to start new cluster with keycloak and all services

---
+++ k8s/start.ps1
@@ -0,0 +1,9 @@
+k3d cluster create newcluster --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer
+
+kubectl create -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes-examples/keycloak.yaml
+
+../gradlew.bat buildFull -p ../
+
+kubectl apply -f ..\dsa\build\kubernetes\kubernetes.yml
+kubectl apply -f ..\math\build\kubernetes\kubernetes.yml
+kubectl apply -f ..\mba\build\kubernetes\kubernetes.yml
