940340a9729f817bdbc7d0b0fedb4af39f5863a5
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-03-13 22:25:06 +0100

    K8S - keycloak ingress set

--- k8s/start.ps1
+++ k8s/start.ps1
@@ -1,6 +1,7 @@
 k3d cluster create newcluster --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer
 
 kubectl create -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes-examples/keycloak.yaml
+kubectl apply -f .\keycloak-ingress.yaml
 
 ../gradlew.bat buildFull -p ../
 
