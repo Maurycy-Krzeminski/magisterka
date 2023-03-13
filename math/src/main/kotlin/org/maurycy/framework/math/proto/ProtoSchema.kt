c39c957a8ee7fc6f8210275ae4e1d73de0b24ee5
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-03-13 18:25:34 +0100

    MATH - removal of not needed classes

--- math/src/main/kotlin/org/maurycy/framework/math/proto/ProtoSchema.kt
+++
@@ -1,9 +0,0 @@
-package org.maurycy.framework.math.proto
-
-import javax.enterprise.context.ApplicationScoped
-import org.infinispan.protostream.GeneratedSchema
-import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder
-
-@ApplicationScoped
-@AutoProtoSchemaBuilder(includeClasses = [], schemaPackageName = "tutorial")
-interface ProtoSchema : GeneratedSchema
